95314bc6d0918ec62a605d043b6bd0686677ad86
chenyuansgit <chenyuan_git@163.com>
2016-08-09 10:18:44 +0800

    常用的功能函数

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # 脚手架 － Express
 保存了项目中常用的代码
 
-1. 
+1. myproject : Express项目
+
+        Config/mysqlConfig.js: Mysql数据库的配置文件
+
+        Common/database/Mysql.js: Mysql数据库连接与请求
+        Common/log/SystemLog.js: 打印日志:颜色
+        Common/log/traceLog.js:  打印日志:所在文件与行
+
+        Common/util/util.js: 常用的功能函数
+
+
+
---
+++ myproject/Common/util/util.js
@@ -0,0 +1,40 @@
+var crypto = require('crypto');
+var fs = require('fs');
+
+var util = {
+    // 生成n位随机数
+    getRand: function(digit) {
+        var number = '';
+        for (var i = 0; i < digit; i++) {
+            number += Math.floor(Math.random() * 10);
+        }
+        //console.log('随机数:', number);
+        return number;
+    },
+
+    // 密码加密
+    cipherPassword: function(password) { // 加密
+        var sha1 = crypto.createHash('sha1');
+        sha1.update(password);
+        var password_cipher = sha1.digest('hex');
+        return password_cipher;
+    },
+
+    // 获取文件的md5
+    getFileMd5: function(filePath, callback){
+        var md5sum = crypto.createHash('md5');
+        var stream = fs.createReadStream(filePath);
+
+        stream.on('data', function (chunk) {
+            md5sum.update(chunk);
+        });
+
+        stream.on('end', function () {
+            var md5 = md5sum.digest('hex').toUpperCase();
+            callback(null, md5);
+        });
+    }
+};
+
+
+module.exports = util;
