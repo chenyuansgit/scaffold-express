eaf9237e97ca1a828eaf5356e7aafd54f708c879
chenyuansgit <chenyuan_git@163.com>
2016-11-21 14:29:36 +0800

    1.设置nginx反向代理: socket.io多进程广播

--- myproject/actions/ReverseProxy/nginx-server/app1.js
+++ myproject/actions/ReverseProxy/nginx-server/app1.js
@@ -1,9 +1,15 @@
 var port = 3100;
 var io = require('socket.io').listen(port);
+
+var redis = require('socket.io-redis');
+io.adapter(redis({ host: 'localhost', port: 6379 }));
+
+
 io.sockets.on('connection', function(socket){
     console.log('new connection...');
     socket.emit('server', {
         server: 'server1',
-        port: port
+        port: port,
+        time: new Date()
     });
 });
--- myproject/actions/ReverseProxy/nginx-server/app2.js
+++ myproject/actions/ReverseProxy/nginx-server/app2.js
@@ -1,9 +1,14 @@
 var port = 3101;
 var io = require('socket.io').listen(port);
+
+var redis = require('socket.io-redis');
+io.adapter(redis({ host: 'localhost', port: 6379 }));
+
 io.sockets.on('connection', function(socket){
     console.log('new connection...');
     socket.emit('server', {
         server: 'server2',
-        port: port
+        port: port,
+        time: new Date()
     });
 });
