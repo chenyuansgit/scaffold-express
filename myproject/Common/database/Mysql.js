4c39accdcb56413e5a0ef52fec734523c525cb75
chenyuansgit <chenyuan_git@163.com>
2016-11-30 15:20:55 +0800

    1.打印mysql sql

--- myproject/Common/database/Mysql.js
+++ myproject/Common/database/Mysql.js
@@ -1,24 +1,31 @@
 var mysql = require('mysql');
-var mysqlConfig = require('../../Config/mysqlConfig.js');
-
+var fs = require('fs');
+var mysqlConfig = require('../../config/mysqlConfig.js');
 
 // 创建连接池
 var pool = mysql.createPool(mysqlConfig);
 
-var query = function(sql, options, callback){
+var query = function(sql, options, callback) {
+    if (typeof options == "function") {
+        throw "第二个参数不能是回调函数";
+    }
     // 从连接池中拿到连接
-    pool.getConnection(function(err, conn){
-        if(err) {
+    pool.getConnection(function(err, conn) {
+        if (err) {
             callback(err, null, null);
             return;
         }
         // 请求数据库
-        conn.query(sql, options, function(err, vals, field ){
+        var query = conn.query(sql, options, function(err, vals, field) {
             //console.log('sql:', sql, options);
             // 释放连接
             conn.release();
             callback(err, vals, field);
         });
+
+        fs.appendFileSync('sql.txt', query.sql + '\n', {encoding: 'utf8'});
+        //console.log(query.sql);
+
     });
 };
 
